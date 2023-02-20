cfa0b7572a2bbaa5a39b6607a9c48d8375d13a76
Burr Sutter <burrsutter@gmail.com>
2023-02-20 17:24:42 -0500

    2 Bonjour

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -34,8 +34,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("hello: " + cnt);
-      return "Hello Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("Bonjour: " + cnt);
+      return "2 Bonjour Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
